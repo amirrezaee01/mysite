1f1dbf1e5d8fb4da5daed035240a4023d1884866
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2024-07-07 10:53:09 +0330

    finsin

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 def blog_view(request):
     return render(request, 'blog/blog-home.html')
 def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+    context = {'writer':'amir mohammad rezaee'}
+    return render(request, 'blog/blog-single.html',context)
