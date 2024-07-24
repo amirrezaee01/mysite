5d4bd3e377142c6ea02d53c15f810707a26cf334
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2024-07-24 11:40:45 +0330

    update pervious and next to this

--- blog/views.py
+++ blog/views.py
@@ -8,13 +8,41 @@ def blog_view(request):
     context = {'posts':posts}
     return render(request, 'blog/blog-home.html',context)
 
-def blog_single(request,pid):
-    posts = Post.objects.filter(status=1)
-    post = get_object_or_404(posts,id=pid)
-    post.counted_views+= 1
+def blog_single(request, pid):
+    # Get all published posts ordered by ID
+    posts = Post.objects.filter(status=1).order_by('id')
+    
+    # Get the current post
+    post = get_object_or_404(posts, id=pid)
+    
+    # Update view count
+    post.counted_views += 1
     post.save()
-    context = {'post':post}
-    return render(request, 'blog/blog-single.html',context)
+    
+    # Find the index of the current post
+    post_ids = [post.id for post in posts]
+    current_index = post_ids.index(post.id)
+    
+    # Determine previous and next posts
+    prev_post = None
+    next_post = None
+    
+    if current_index > 0:
+        prev_post_id = post_ids[current_index - 1]
+        prev_post = get_object_or_404(Post, id=prev_post_id, status=1)
+    
+    if current_index < len(post_ids) - 1:
+        next_post_id = post_ids[current_index + 1]
+        next_post = get_object_or_404(Post, id=next_post_id, status=1)
+    
+    # Prepare context
+    context = {
+        'post': post,
+        'prev_post': prev_post,
+        'next_post': next_post,
+    }
+    
+    return render(request, 'blog/blog-single.html', context)
 
 def test(request,pid):
     post = get_object_or_404(Post,id=pid)
