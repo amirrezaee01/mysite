1cfd7e8cea78983842201844f75064b87f44f032
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2024-07-31 14:10:28 +0330

    email input become correct

--- website/admin.py
+++ website/admin.py
@@ -3,7 +3,7 @@ from website.models import Contact,Newsletter
 
 class ContatcAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
-    list_display = ('name','email','subject')
+    list_display = ('name','email','subject','massage')
     list_filter = ['email']
     search_fields = ('name','massage')
     
--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from website.models import Contact
+from django.contrib import messages
 from website.forms import Nameform,ContactForm,NewsletterForm
 
 def index_view(request):
@@ -13,10 +14,13 @@ def contact_view(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.add_message(request,messages.SUCCESS,'your ticket submit it correctly')
         else:
-            return HttpResponse('you have problem')
+            messages.add_message(request,messages.ERROR,'your ticket didnt submit correctly')
+            
     form = ContactForm()
     return render(request, 'website/contact.html', {'form':form})
+
 def newsletter_view(request):
     if request.method == 'POST':
         form = NewsletterForm(request.POST)
